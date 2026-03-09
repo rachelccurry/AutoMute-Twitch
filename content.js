9397eab66290cc2a61deafdec776c5de38d01d0b
Rachel Curry <rachelcxd@gmail.com>
2026-03-09 09:26:34 -0700

    making a bit cleaner-looking

--- content.js
+++ content.js
@@ -1,23 +1,65 @@
+// Rachel Curry
+// Last Updated: 3/9/26
 // automatically mutes video if twitch ad pops up
-// this should not affect anything if the user touches the video volume
 // doesn't do sidebar ads because those are auto-muted already
-
-// for testing
-console.log("RUNNING RN");
 
 let recentVolume = 0.5;
 let isMutedByMe = false;
 let mutedVideo = null // to check if it's the same video 
 
-// get video object
 function getVideo() {
     return document.querySelector("video")
 }
-
 // check if ad label exists (on Twitch, doesn't exist on regular vid)
 function adLabelPresent() {
   return !!document.querySelector('[data-a-target="video-ad-label"]');
 }
+function mute(video) {
+    // if I previously muted and the video is unmuted (user changed state)
+    // if (isMutedByMe && !video.muted) {
+    //     isMutedByMe = false;
+    //     mutedVideo = null;
+    //     return;
+    // }
+ 
+    if (isMutedByMe) return;
+
+    recentVolume = video.volume;
+    mutedVideo = video;
+    video.muted = true;
+    isMutedByMe = true;
+    console.log('Ad detected. Muting.')
+}
+function unmute(video) {
+    // if I didn't previously mute
+    if (!isMutedByMe) return;
+
+    // if the video is muted but it's not the ad (user changed state)
+    //if (video.muted && video !==mutedVideo) return;
+
+    video.muted = false;
+    video.volume = recentVolume;
+    isMutedByMe = false;
+    mutedVideo = null;
+    console.log('Ad is over!')
+}
+function handleChange() {
+    const video = getVideo();
+    if (!video) return;
+
+    if (adLabelPresent(video)){
+        mute(video);
+    }
+    else {
+        unmute(video);
+    }
+}
+
+setInterval(handleChange, 1000);
+handleChange();
+
+
+// Unused atm
 
 // get rid of prob
 // function adPlaying(video) {
@@ -34,52 +76,6 @@ function adLabelPresent() {
 //     );
 // }
 
-function mute(video) {
-    // if I previously muted and the video is unmuted (user changed state)
-    // if (isMutedByMe && !video.muted) {
-    //     isMutedByMe = false;
-    //     mutedVideo = null;
-    //     return;
-    // }
- 
-    // if I muted already and is still muted
-    if (isMutedByMe) return;
-
-    // otherwise mute video
-    recentVolume = video.volume;
-    mutedVideo = video;
-    video.muted = true;
-    isMutedByMe = true;
-    console.log('Ad detected. Muting.')
-}
-
-function unmute(video) {
-    // if I didn't previously mute
-    if (!isMutedByMe) return;
-
-    // if the video is muted but it's not the ad (user changed state)
-    //if (video.muted && video !==mutedVideo) return;
-
-    // otherwise unmute video
-    video.muted = false;
-    video.volume = recentVolume;
-    isMutedByMe = false;
-    mutedVideo = null;
-    console.log('Ad is over!')
-}
-
-function handleChange() {
-    const video = getVideo();
-    if (!video) return;
-
-    if (adLabelPresent(video)){
-        mute(video);
-    }
-    else {
-        unmute(video);
-    }
-}
-
 // const observer = new MutationObserver(handleChange);
 
 // observer.observe(document.body, {
@@ -88,7 +84,3 @@ function handleChange() {
 //     attributes: true,
 //     attributeFilter: ["data-a-target", "src"]
 // });
-
-setInterval(handleChange, 1000);
-
-handleChange();
